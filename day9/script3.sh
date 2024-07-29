3623199d5c6bcbd98790ab406671d621136622d9
Manan Taori <taorimanan19@gmail.com>
2024-07-29 16:19:59 +0530

    day 14 task added

--- day9/script3.sh
+++ day9/script3.sh
@@ -1,67 +1,73 @@
 #!/bin/bash
 LOG_FILE="/var/log/syslog.log"
-REPORT_FILE="/home/einfochips/Desktop/report_$(date +'%Y%m%d_%H%M%S').txt"  
-THRESHOLD_CPU=90  
-THRESHOLD_MEM=90  
+REPORT_FILE="/home/einfochips/Desktop/report_$(date +'%Y%m%d_%H%M%S').txt"
+THRESHOLD_CPU=90
+THRESHOLD_MEM=90
 
-# if [ "$(id -u)" != "0" ]; then
-#     echo "Script must be run as root."
-#     exit 1
-# fi
-dependencies=( "awk" "grep" "sed" "df" "top" "netstat" "service" "ping" )
+dependencies=( "awk" "grep" "sed" "df" "top" "netstat" "ping" )
 for cmd in "${dependencies[@]}"; do
     if ! command -v "$cmd" &> /dev/null; then
         echo "$cmd command not found. Please install $cmd."
         exit 1
     fi
 done
+
 monitor_cpu() {
     echo "### CPU Usage ###" >> "$REPORT_FILE"
-    top -b -n 1 | head -n 20 >> "$REPORT_FILE"
+    /usr/bin/top -b -n 1 | head -n 20 >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 monitor_memory() {
     echo "### Memory Usage ###" >> "$REPORT_FILE"
-    free -h >> "$REPORT_FILE"
+    /usr/bin/free -h >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 monitor_disk() {
     echo "### Disk Usage ###" >> "$REPORT_FILE"
-    df -h >> "$REPORT_FILE"
+    /bin/df -h >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 monitor_network() {
     echo "### Network Statistics ###" >> "$REPORT_FILE"
-    netstat -s >> "$REPORT_FILE"
+    /bin/netstat -s >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 capture_top_processes() {
     echo "### Top Processes ###" >> "$REPORT_FILE"
-    ps aux --sort=-%cpu | head -n 10 >> "$REPORT_FILE"
+    /bin/ps aux --sort=-%cpu | head -n 10 >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 parse_logs() {
     echo "### System Log Analysis ###" >> "$REPORT_FILE"
-    tail -n 100 /var/log/syslog | grep -i "error\|failed" >> "$REPORT_FILE"
+    /usr/bin/tail -n 100 /var/log/syslog | /bin/grep -i "error\|failed" >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 check_services() {
     echo "### Service Status ###" >> "$REPORT_FILE"
-    service apache2 status >> "$REPORT_FILE"
-    service mysql status >> "$REPORT_FILE"
+    systemctl status apache2 >> "$REPORT_FILE"
+    systemctl status mysql >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 check_connectivity() {
     echo "### Connectivity Check ###" >> "$REPORT_FILE"
-    ping -c 5 google.com >> "$REPORT_FILE"
+    /bin/ping -c 5 google.com >> "$REPORT_FILE"
     echo "" >> "$REPORT_FILE"
 }
+
 send_alert() {
     subject="ALERT: High resource utilization on $(hostname)"
-    # mail -s "$subject" mtaori197@gmail.com < "$REPORT_FILE"
+    # /usr/bin/mail -s "$subject" mtaori197@gmail.com < "$REPORT_FILE"
 }
+
 generate_report_filename() {
-    REPORT_FILE="/home/einfochips/Desktop/report_$(date +'%Y%m%d_%H%M%S').txt"
+    REPORT_FILE="/home/einfochips/Desktop/system_report_$(date +'%Y%m%d_%H%M%S').txt"
 }
 
 generate_report() {
@@ -77,18 +83,20 @@ generate_report() {
     check_connectivity
     echo "Report generated on $(date)" >> "$REPORT_FILE"
 }
+
 main() {
     generate_report
-    cpu_usage=$(top -b -n 1 | awk '/^%Cpu/{print 100 -$8}')
-    mem_usage=$(free | awk '/Mem/{print $3/$2 * 100.0}')
+    cpu_usage=$(/usr/bin/top -b -n 1 | /usr/bin/awk '/^%Cpu/{print 100 -$8}')
+    mem_usage=$(/usr/bin/free | /usr/bin/awk '/Mem/{print $3/$2 * 100.0}')
     
-    if (( $(bc <<< "$cpu_usage >= $THRESHOLD_CPU") )); then
+    if (( $(/usr/bin/bc <<< "$cpu_usage >= $THRESHOLD_CPU") )); then
         send_alert
     fi
     
-    if (( $(bc <<< "$mem_usage >= $THRESHOLD_MEM") )); then
+    if (( $(/usr/bin/bc <<< "$mem_usage >= $THRESHOLD_MEM") )); then
         send_alert
     fi
 }
+
 main
 exit 0
